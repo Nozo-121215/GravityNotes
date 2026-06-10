dd452f6b0a38ab35e0f1c302faa22f72677f8d8b
realryo1 <realryo1@gmail.com>
2026-06-10 11:57:14 +0900

    debugscene

---
+++ debugscene/debugscene.cpp
@@ -0,0 +1,33 @@
+﻿#include "debugscene.h"
+#include "sprite2d.h"
+#include "texture.h"
+#include "keyboard.h"
+#include "fade.h"
+#include "debug_ostream.h"
+#include "define.h"
+#include "font.h"
+#include "mouse.h"
+#include "sound.h"
+#include "ClickFont.h"
+
+using namespace DirectX;
+
+void DebugScene_Initialize(void)
+{
+
+}
+
+void DebugScene_Update(void)
+{
+
+}
+
+void DebugScene_Draw(void)
+{
+
+}
+
+void DebugScene_Finalize(void)
+{
+
+}
---
+++ debugscene/debugscene.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+#include <d3d11.h>
+#include <DirectXMath.h>
+using namespace DirectX;
+
+void DebugScene_Initialize(void);
+void DebugScene_Update(void);
+void DebugScene_Draw(void);
+void DebugScene_Finalize(void);
